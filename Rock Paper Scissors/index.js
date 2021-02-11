7df4e68f5745d65b6e01964fec7b582989a9ad2d
Gogi <gogion@protonmail.com>
2021-02-11 09:44:30 +0200

    created function playRound() + seems to be working

--- Rock Paper Scissors/index.js
+++ Rock Paper Scissors/index.js
@@ -1,19 +1,77 @@
+const playerSelection = 'RoCK'
+const computerSelection = computerPlay()
+console.log(playRound(playerSelection, computerSelection))
+
+function playRound(playerSelection, computerSelection) {
+  playerSelectionLowerCase = playerSelection.toLowerCase()
+  // debugger
+  console.log(`User: ${playerSelectionLowerCase}`)
+
+  //   need to determine win or lose.
+  // if statement?
+  // rock > scissors  |   scissors > paper  | paper > rock
+
+  let result
+
+  if (playerSelectionLowerCase === 'rock' && computerSelection === 'paper') {
+    result = `You Lose! ${computerSelection} beats ${playerSelectionLowerCase}`
+  } else if (playerSelectionLowerCase === 'paper' && computerSelection === 'paper') {
+    result = "It's a draw"
+  } else if (playerSelectionLowerCase === 'scissors' && computerSelection === 'paper') {
+    result = `You Win! ${playerSelectionLowerCase} beats ${computerSelection}`
+  } else if (playerSelectionLowerCase === 'rock' && computerSelection === 'scissors') {
+    result = `You Win! ${playerSelectionLowerCase} beats ${computerSelection}`
+  } else if (playerSelectionLowerCase === 'paper' && computerSelection === 'scissors') {
+    result = `You Lose! ${computerSelection} beats ${playerSelectionLowerCase}`
+  } else if (playerSelectionLowerCase === 'scissors' && computerSelection === 'scissors') {
+    result = "It's a draw"
+  } else if (playerSelectionLowerCase === 'rock' && computerSelection === 'rock') {
+    result = "It's a draw"
+  } else if (playerSelectionLowerCase === 'paper' && computerSelection === 'rock') {
+    result = `You Win! ${playerSelectionLowerCase} beats ${computerSelection}`
+  } else if (playerSelectionLowerCase === 'scissors' && computerSelection === 'rock') {
+    result = `You Lose! ${computerSelection} beats ${playerSelectionLowerCase}`
+  }
+
+  //   switch ((playerSelection, computerSelection)) {
+  //     //   debugger
+  //     case 'rock', 'paper':
+  //       console.log('lose')
+  //       break
+  //     // case 2:
+  //     //   // computerRound = 'Paper'
+  //     //   console.log('Computer: Paper')
+  //     //   break
+  //     // case 3:
+  //     //   // computerRound = 'Scissors'
+  //     //   console.log('Computer: Scissors')
+  //     //   break
+  //   }
+  //   return `You Win! ${playerSelection} beats ${computerSelection}`
+  //   return 'You Lose! Paper beats Rock'
+  return result
+}
+
 function computerPlay() {
   // creating random number from 1 to 3
   randomNum = Math.floor(Math.random() * 3 + 1)
 
+  //   let computerRound
   //   switch statement to determine what the above random number represents
   switch (randomNum) {
     case 1:
-      console.log('Rock')
+      console.log('Computer: rock')
+      return 'rock'
       break
     case 2:
-      console.log('Paper')
+      console.log('Computer: paper')
+      return 'paper'
       break
     case 3:
-      console.log('Scissors')
+      console.log('Computer: scissors')
+      return 'scissors'
       break
   }
+
+  //   return computerRound
 }
-// calling the above function
-computerPlay()
