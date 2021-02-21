2d75931d3fec79bbf504489300914e3a0bf52a15
Gogi <gogion@protonmail.com>
2021-02-21 23:15:49 +0200

    completed repeatString.js

--- Fundamentals 4/repeatString/repeatString.js
+++ Fundamentals 4/repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, times) {
+    let result;
+    if (times > 0) {
+        result = str.repeat(times);
+    } else if (times === 0) {
+        result = '';
+    } else {
+        result = 'ERROR';
+    }
+    console.log(result);
+    return result;
+};
 
-}
-
-module.exports = repeatString
+module.exports = repeatString;
