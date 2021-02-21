b3006d87b091480a98ff81d83eaebe984b29f957
Gogi <gogion@protonmail.com>
2021-02-21 21:08:36 +0200

    completed 1st excersise in removeFromArray

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, element) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            array.splice(i, 1);
+        }
+    }
+    return array;
+};
 
-}
-
-module.exports = removeFromArray
+module.exports = removeFromArray;
