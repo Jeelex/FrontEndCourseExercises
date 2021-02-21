17e3899ce6df728ce4f600b1bca7f251fb843d5f
Gogi <gogion@protonmail.com>
2021-02-21 22:49:43 +0200

    completed removeFromArray.js

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,9 +1,21 @@
-const removeFromArray = function (array, element) {
+const removeFromArray = function (array, ...elements) {
+    console.log('original array: ' + array);
     for (let i = 0; i < array.length; i++) {
-        if (array[i] === element) {
-            array.splice(i, 1);
+        for (let j = 0; j < elements.length; j++) {
+            // if (array[i] !== elements[j]) continue;
+
+            if (array[i] === elements[j]) {
+                // console.log('i before : ' + i);
+                array.splice(i, 1);
+                i--;
+                // console.log('i after : ' + i);
+            }
+            // else {
+            //     console.log('did not match element!');
+            // }
         }
     }
+    // console.log('final array: ' + array);
     return array;
 };
 
