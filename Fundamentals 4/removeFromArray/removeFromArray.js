52b0d4570d8ec558195f05a09e79b03bec205f3c
Gogi <gogion@protonmail.com>
2021-02-22 23:16:56 +0200

    removed console.logs from removeFromArray.js

--- Fundamentals 4/removeFromArray/removeFromArray.js
+++ Fundamentals 4/removeFromArray/removeFromArray.js
@@ -1,21 +1,12 @@
 const removeFromArray = function (array, ...elements) {
-    console.log('original array: ' + array);
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < elements.length; j++) {
-            // if (array[i] !== elements[j]) continue;
-
             if (array[i] === elements[j]) {
-                // console.log('i before : ' + i);
                 array.splice(i, 1);
                 i--;
-                // console.log('i after : ' + i);
             }
-            // else {
-            //     console.log('did not match element!');
-            // }
         }
     }
-    // console.log('final array: ' + array);
     return array;
 };
 
