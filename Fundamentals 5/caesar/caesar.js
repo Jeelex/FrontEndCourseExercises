613de040418f586a968f696c1298d79a12628055
Gogi <gogion@protonmail.com>
2021-03-06 00:23:48 +0200

    separated uppercase and lowercase letters

--- Fundamentals 5/caesar/caesar.js
+++ Fundamentals 5/caesar/caesar.js
@@ -1,49 +1,60 @@
-const caesar = function(userInput, shiftNumber) {
-    // we need to return the ENCODED STRING
-    // turn userInput into charCodeAt()
-    // const charCode = userInput.charCodeAt(0);
-    // // add or subtract the shiftNumber
-    // const finalCharCode = charCode + shiftNumber;
-    // // return the result
-    // return String.fromCharCode(finalCharCode);
-    let result = "";
+const caesar = function (userInput, shiftNumber) {
+	// we need to return the ENCODED STRING
+	// turn userInput into charCodeAt()
+	// const charCode = userInput.charCodeAt(0);
+	// // add or subtract the shiftNumber
+	// const finalCharCode = charCode + shiftNumber;
+	// // return the result
+	// return String.fromCharCode(finalCharCode);
+	let result = '';
 
-    for (let i = 0; i < userInput.length; i++) {
-        if (userInput.charCodeAt(i) >= 65 && userInput.charCodeAt(i) <= 122) {
-            const charCode = userInput.charCodeAt(i);
-            result += String.fromCharCode(charCode + shiftNumber);
-        }   else {
-            result += userInput.charAt(i);
-        }
-            
-    }
+	for (let i = 0; i < userInput.length; i++) {
+		// check if shiftNumber is a negative number
+		if (shiftNumber < 0) {
+			shiftNumber += 26 + (shiftNumber % 26);
+		}
 
-    return result;
-    // function convertToUnicode(input) {
-    //     input.charCodeAt();
-    //     charCode + shiftNumber;
-    // }
+		// Uppercase letters
+		if (userInput.charCodeAt(i) >= 65 && userInput.charCodeAt(i) <= 90) {
+			const charCode = userInput.charCodeAt(i);
+			result += String.fromCharCode(((charCode - 65 + shiftNumber) % 26) + 65);
 
-    // const userInputArray = Array.from(userInput);
-    // const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+			// Lowercase letters
+		} else if (userInput.charCodeAt(i) >= 97 && userInput.charCodeAt(i) <= 122) {
+			const charCode = userInput.charCodeAt(i);
+			result += String.fromCharCode(((charCode - 97 + shiftNumber) % 26) + 97);
 
-    // for (let i = 0; i < userInputArray.length; i++) {
-    //     console.log(i);
-        
-    // };
+			// if it's not a letter
+		} else {
+			result += userInput.charAt(i);
+		}
+	}
 
-    // let currentIndex = alphabet[];
+	return result;
+	// function convertToUnicode(input) {
+	//     input.charCodeAt();
+	//     charCode + shiftNumber;
+	// }
 
-    // const newArray = userInputArray.map(() => {
-    //     if (shiftNumber > 0){
-    //         return alphabet[shiftNumber -1];
-    //     } else {
-    //         return alphabet[shiftNumber -1];
-    //     }
-    // });
+	// const userInputArray = Array.from(userInput);
+	// const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-    // return newArray;
+	// for (let i = 0; i < userInputArray.length; i++) {
+	//     console.log(i);
 
-}
+	// };
 
-module.exports = caesar
+	// let currentIndex = alphabet[];
+
+	// const newArray = userInputArray.map(() => {
+	//     if (shiftNumber > 0){
+	//         return alphabet[shiftNumber -1];
+	//     } else {
+	//         return alphabet[shiftNumber -1];
+	//     }
+	// });
+
+	// return newArray;
+};
+
+module.exports = caesar;
--- Fundamentals 5/caesar/caesar.spec.js
+++ Fundamentals 5/caesar/caesar.spec.js
@@ -10,7 +10,7 @@ describe('caesar', function() {
   it('works with phrases', function() {
     expect(caesar('Hello, World!', 5)).toEqual('Mjqqt, Btwqi!');
   });
-  xit('works with negative shift', function() {
+  it('works with negative shift', function() {
     expect(caesar('Mjqqt, Btwqi!', -5)).toEqual('Hello, World!');
   });
   xit('wraps', function() {
