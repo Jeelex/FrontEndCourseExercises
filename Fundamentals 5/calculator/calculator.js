75640f4cd70681902abbc1f5ec8428f970686dbb
Gogi <gogion@protonmail.com>
2021-03-09 19:32:53 +0200

    code tidying in calculator.js

--- Fundamentals 5/calculator/calculator.js
+++ Fundamentals 5/calculator/calculator.js
@@ -10,11 +10,6 @@ function sum (array) {
 		return array.reduce((total, num) => {
 			return total + num;
 		}, 0);
-		// let result = 0;
-		// for (let i =0; i<array.length; i++){
-		// 	result += array[i];
-		// }
-		// return result;
 }
 
 function multiply (array) {
@@ -31,7 +26,6 @@ function factorial(n) {
 	if (n === 0 || n === 1) {
 		return 1;
 	}
-	// let result = 1;
 	for (let i = n-1; i >= 1; i--) {
 		n *= i;
 	}
