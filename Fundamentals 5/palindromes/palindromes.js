2b69123faddb318cc74c4ac4a785bdca39ffa35c
Gogi <gogion@protonmail.com>
2021-03-09 19:27:53 +0200

    completed palindromes.js

--- Fundamentals 5/palindromes/palindromes.js
+++ Fundamentals 5/palindromes/palindromes.js
@@ -1,11 +1,9 @@
 const palindromes = function(str) {
-    const strLowerCase = str.toLowerCase();
-    const strReversed = str.split('').reverse().join('');
-    if (strReversed === strLowerCase){
-        return true;
-    } else {
-        return false;
-    }
+    const punctuation = /[^A-Za-z0-9]/g;
+    const strLowNoPunct = str.toLowerCase().replace(punctuation, "");
+    const strReversed = strLowNoPunct.split('').reverse().join('');
+
+    return strLowNoPunct === strReversed;
 }
 
 module.exports = palindromes
