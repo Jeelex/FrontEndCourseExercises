289a5ec89e7f9d9b924c0bdea9b2f2c200c5e7be
Gogi <gogion@protonmail.com>
2021-03-31 23:31:54 +0300

    added javascript file to populate nav-top and nav-bottom

---
+++ Building with Backgrounds and Gradients/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const footerGridContainer = document.getElementById("ft-grid-container");
+const topNavList = document.getElementById("top-nav-list");
+const bottomNavList = document.getElementById("bottom-nav-list");
+
+
+const navItemsTop = ["Store", "Mac", "iPod", "iPhone", "iPad", "itunes", "Support"];
+
+const navItemsBottom = ["Site Map", "Hot New", "RSS Feeds", "Media info", "Environment", "Job Opportunities", "Contact Us"];
+
+function addCategoryLink(array, list) {
+	for (let i = 0; i < array.length; i++) {
+		const html = `<li class="nav-link"><a class="a-links-no-decoration" href="#">${array[i]}</a></li>`;
+		list.insertAdjacentHTML("beforeend", html);
+	}
+}
+
+addCategoryLink(navItemsTop, bottomNavList);
+addCategoryLink(navItemsBottom, topNavList);
