5720ac656a99c421986e1906a122b0f01c9d9387
Gogi <gogion@protonmail.com>
2021-04-19 14:01:51 +0300

    completed js-object-contructor exercise

---
+++ JS-object-constructors/index.js
@@ -0,0 +1,17 @@
+function Book(title, author, pages, isRead) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.isRead = isRead;
+	this.info = function () {
+		if (!isRead) {
+			return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+		} else {
+			return `${this.title} by ${this.author}, ${this.pages} pages, has been read`;
+		}
+	};
+}
+
+const myBook = new Book("Harry Potter", "J. K. Rowling", 495, true);
+
+console.log(myBook.info());
