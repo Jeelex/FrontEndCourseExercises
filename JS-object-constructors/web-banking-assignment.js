d19845c34ec6e4f5cfebeba6d0607651e90fdcc6
Gogi <gogion@protonmail.com>
2021-04-22 00:40:37 +0300

    completed web banking assignment except creating new IBAN for each customer

--- JS-object-constructors/web-banking-assignment.js
+++ JS-object-constructors/web-banking-assignment.js
@@ -1,42 +1,37 @@
-// Create a Function constructor that creates Bank Accounts
-
-// Implement the following prototype methods:
-
-// Validation rules include:
-// 2.2.1. Amount must not be negative number
-// 2.2.2. Amount must be of type Number
-// 2.2.3. Amount must not exceed current balance
+"use strict";
 
 function Account(fullname) {
 	this.fullname = fullname;
-	// Automatically give a new IBAN number to new accounts. (Static property)
-	this.IBAN = "GR00010003";
-	this.balance = 0;
 
-	// 2.1. deposit: will deposit an amount to the current balance
+	this.balance = 0;
 	this.deposit = function (amount) {
-		return console.log(amount);
+		return this.balance = this.balance + amount;
 	};
-	// 2.2. withdraw: will withdraw an amount from the current balance an return the withdrawn amount
 	this.withdraw = function (amount) {
-		return console.log(amount);
+    if (typeof amount !== "number" || amount <= 0){
+      return "Error 'Invalid amount'"
+    }
+    if (amount > this.balance) {
+      return "Error 'Insufficient balance!'"
+    }
+		return this.balance = this.balance - amount;
 	};
-	// 2.3. getBalance: will return the current balance
 	this.getBalance = function () {
-		return console.log(this.balance);
+		return this.balance;
 	};
 }
+
+
 
 const newAccount = new Account("Kostas Minaidis");
 // New account created for: Kostas Minaidis
 // IBAN: GR00010003
 
-newAccount.getBalance(); // 0
-newAccount.deposit(100);
-newAccount.getBalance(); // 100
-newAccount.withdraw(50);
-newAccount.getBalance(); // 50
+console.log({ newAccount });
 
-// newAccount.withdraw( 500 )   // Error 'Insufficient balance!'
-// newAccount.withdraw( "50" )  // Error 'Invalid amount'
-// newAccount.withdraw( -150 )  // Error 'Invalid amount'
+console.log( newAccount.getBalance() ); // 0
+newAccount.deposit(100);
+console.log( newAccount.getBalance() ); // 100
+console.log(newAccount.withdraw(50));
+// newAccount.withdraw(50);
+console.log( newAccount.getBalance() ); // 50
