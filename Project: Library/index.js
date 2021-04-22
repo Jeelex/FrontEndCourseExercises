75f0f06a0dee73462c4575ddd5e0690d4fed7718
Gogi <gogion@protonmail.com>
2021-04-23 01:59:31 +0300

    changed boolean property name

--- Project: Library/index.js
+++ Project: Library/index.js
@@ -1,14 +1,13 @@
-
 let myLibrary = [];
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, hasBeenRead) {
 	this.title = title;
 	this.author = author;
 	this.pages = pages;
-	this.isRead = isRead;
+	this.hasBeenRead = hasBeenRead;
 	this.info = function () {
-		if (!isRead) {
-			return `${this.title} by ${this.author}, ${this.pages} pages, not read yet`;
+		if (!hasBeenRead) {
+			return `${this.title} by ${this.author}, ${this.pages} pages, has not been read yet`;
 		} else {
 			return `${this.title} by ${this.author}, ${this.pages} pages, has been read`;
 		}
@@ -19,7 +18,6 @@ const myBook = new Book("Harry Potter", "J. K. Rowling", 495, true);
 
 console.log(myBook.info());
 
-
 function addBookToLibrary() {
-  // do stuff here
+	// do stuff here
 }
