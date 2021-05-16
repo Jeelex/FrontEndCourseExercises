cdc3aafa42b3ed170bbd32a6dff7080d2eae2315
Gogi <gogion@protonmail.com>
2021-05-16 03:44:45 +0300

    removed border-bottom from <td>

--- JavaScript/TodoList/todo/src/todo-constructor.js
+++ JavaScript/TodoList/todo/src/todo-constructor.js
@@ -10,15 +10,15 @@ Todo.prototype.addToPage = function () {
 	const el = document.createElement("tr");
 	const table = document.getElementById("tasks");
 
-	table.setAttribute("class", "mx-auto");
+	table.setAttribute("class", "mx-auto ");
 
 	el.innerHTML = `
-          <td class="text">${this.title}</td>
-          <td class="text">${this.description}</td>
-          <td class="text">${this.dueDate}</td>
-          <td class="text">${this.priority}</td>
-          <td class="text"><button type="button" class="edit btn btn-info">Edit</button></td>
-          <td class="text"><button type="button" class="delete btn btn-danger">Delete</button></td>
+          <td class="text border-0">${this.title}</td>
+          <td class="text border-0">${this.description}</td>
+          <td class="text border-0">${this.dueDate}</td>
+          <td class="text border-0">${this.priority}</td>
+          <td class="text border-0"><button type="button" class="edit btn btn-info">Edit</button></td>
+          <td class="text border-0"><button type="button" class="delete btn btn-danger">Delete</button></td>
         `;
 	table.appendChild(el);
 
